6b95644f31c7e31551aa8ca4385e9aa960cef83d
Tewson Seeoun <tewson.seeoun@gmail.com>
2020-03-14 23:22:45 +0000

    Add a minimal Apollo server serving mock data

---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type House {
+    uri: String
+  }
+  type Query {
+    house: House
+  }
+`;
+
+const demoHouse = {
+  uri: "https://data.oireachtas.ie/ie/oireachtas/house/dail/33"
+};
+
+const resolvers = {
+  Query: {
+    house: () => demoHouse
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
