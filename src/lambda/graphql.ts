2c4dd6fbceff6c4045260b0c65dc6d32261dc3bd
Tewson Seeoun <tewson.seeoun@gmail.com>
2020-05-12 01:09:55 +0100

    Add an example query

--- src/lambda/graphql.ts
+++ src/lambda/graphql.ts
@@ -2,6 +2,30 @@ import { ApolloServer } from "apollo-server-lambda";
 
 import { config } from "../config";
 
-const server = new ApolloServer(config);
+const examplePlaygroundQuery = `# Query full names of all members of the 33rd Dáil
+{
+  house(type: dail, term: "33") {
+    showAs
+    members {
+      fullName
+    }
+  }
+}
+`;
+
+const configWithPlayground = {
+  ...config,
+  playground: {
+    tabs: [
+      {
+        endpoint: process.env.DEPLOY_URL,
+        name: "Example query",
+        query: examplePlaygroundQuery
+      }
+    ]
+  }
+};
+
+const server = new ApolloServer(configWithPlayground);
 
 export const handler = server.createHandler();
